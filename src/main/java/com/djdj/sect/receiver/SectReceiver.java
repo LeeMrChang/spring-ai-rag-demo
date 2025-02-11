0fa53efe98f023f482c9e8bef451d83b477f3bad
lichanghao <lichanghao@djdj.cc>
2025-02-11 16:29:51 +0800

    fix:编写测试用例代码提交

--- src/main/java/com/djdj/sect/receiver/SectReceiver.java
+++ src/main/java/com/djdj/sect/receiver/SectReceiver.java
@@ -1,17 +1,15 @@
 package com.djdj.sect.receiver;
 
-import cn.hutool.core.date.DateUtil;
 import cn.hutool.json.JSONArray;
 import cn.hutool.json.JSONUtil;
 import com.djdj.sect.entity.Sect;
 import com.djdj.sect.service.ISectService;
 import jakarta.annotation.Resource;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
-
-import java.util.List;
 
 /**
  * @author lichanghao
@@ -27,19 +25,12 @@ public class SectReceiver {
     private ISectService sectService;
 
     @RabbitHandler
+    @SneakyThrows
     public void process(String jsonStr) {
         log.info("SectReceiver消费者收到消息: {}", jsonStr);
         sectService.truncateTable();
         JSONArray jsonArray = new JSONArray(jsonStr);
-        List<Sect> list = jsonArray.toList(Sect.class);
-        list.forEach(sect -> {
-            sect.setCreateId(0L);
-            sect.setCreateTime(DateUtil.date());
-            sect.setUpdateId(0L);
-            sect.setUpdateTime(DateUtil.date());
-            sect.setDelFlag(false);
-        });
-        sectService.saveBatch(list);
+        sectService.saveBatch(jsonArray.toList(Sect.class));
         log.info("SectReceiver消费消息  : {}", JSONUtil.toJsonStr(jsonStr));
     }
 }
